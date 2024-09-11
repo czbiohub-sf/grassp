f10de6ab1da2842f2c371533b876825134ddcc82
Max Frank <max.frank@czbiohub.org>
2024-09-11 09:38:53 -0700

    add dtype to read and make sure var keeps the right dtypes

--- orgipy/io/read.py
+++ orgipy/io/read.py
@@ -3,18 +3,23 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     import alphastats
+    from typing import Union
 
 import re
 
 import anndata
+import numpy as np
+import pandas as pd
 
 
-def read(loader: alphastats.BaseLoader) -> anndata.AnnData:
+def read(
+    loader: alphastats.BaseLoader, x_dtype: Union[np.dtype, type, int, float, None] = None
+) -> anndata.AnnData:
     import alphastats
 
     alphastats.DataSet._check_loader(
         1, loader
-    )  # need to put ugly one because this is not specified as a staticmethod in alphapeptstats
+    )  # need to put ugly 1 because this is not specified as a staticmethod in alphapeptstats
     rawinput = loader.rawinput
     software = loader.software
     index_column = loader.index_column
@@ -34,10 +39,11 @@ def read(loader: alphastats.BaseLoader) -> anndata.AnnData:
         intensity_col_mask = df.columns.isin(intensity_column)
 
     # Convert to anndata object
-    df = df.transpose()
-    var = df[~intensity_col_mask].transpose()
+    var = df.loc[:, ~intensity_col_mask]
+    X = df.loc[:, intensity_col_mask]
+    obs = pd.DataFrame(index=X.columns)
     var.set_index(index_column, inplace=True)
-    adata = anndata.AnnData(X=df[intensity_col_mask], var=var)
+    adata = anndata.AnnData(X=X.to_numpy(dtype=x_dtype).T, var=var, obs=obs)
     adata.obs["Intensity_col"] = adata.obs.index
     sample_regex = re.compile(intensity_column.replace("[sample]", ""))
     adata.obs["Sample_name"] = adata.obs.index.str.replace(sample_regex, "", regex=True)
