1aa5cf278f174092da9266f2b48aa7894feac7fd
Max Frank <max.frank@czbiohub.org>
2024-10-21 17:41:05 -0700

    add volcano plot

--- orgipy/plotting/__init__.py
+++ orgipy/plotting/__init__.py
@@ -1,3 +1,4 @@
 # from .heatmaps import grouped_heatmap
+from .heatmaps import protein_clustermap
 from .integration import aligned_umap, remodeling_sankey, remodeling_score
-from .qc import highly_variable_proteins
+from .qc import bait_volcano_plots, highly_variable_proteins
