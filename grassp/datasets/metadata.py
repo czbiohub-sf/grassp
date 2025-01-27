9d37ca0ecc7c4d9537bf6c52d10faecd6be65022
Max Frank <max.frank@czbiohub.org>
2025-01-27 11:21:03 -0800

    change subcellular metadata name

--- grassp/datasets/metadata.py
+++ grassp/datasets/metadata.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 
 def subcellular_annotations() -> pd.DataFrame:
-    return pd.read_csv(Path(__file__).parent / "external/subcellular_annotations.tsv", sep="\t")
+    return pd.read_csv(Path(__file__).parent / "external/subcellular_annotations.tsv", sep="\t", index_col=0)
