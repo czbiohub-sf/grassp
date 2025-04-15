3791c5afc26092b4fd80fe60f20bae65c47852d4
Max Frank <max.frank@czbiohub.org>
2025-04-15 15:35:13 -0700

    fetch proloc data directly from their github

--- grassp/datasets/__init__.py
+++ grassp/datasets/__init__.py
@@ -1,2 +1,7 @@
-from .example_data import ithzak_2016, schlessner_2023
+from .example_data import (
+    ithzak_2016,
+    schlessner_2023,
+    download_prolocdata,
+    list_prolocdata_files,
+)
 from .metadata import subcellular_annotations, uniprot_compartment_goterms_path
--- grassp/datasets/example_data.py
+++ grassp/datasets/example_data.py
@@ -5,6 +5,9 @@ if TYPE_CHECKING:
     from anndata import AnnData
 
 import scanpy
+import requests
+import os
+from .. import io
 
 from scanpy._settings import settings
 
@@ -19,3 +22,32 @@ def schlessner_2023() -> AnnData:
     filename = settings.datasetdir / "schlessner_2023.h5ad"
     url = "https://drive.google.com/uc?export=download&id=1JMHWDqLeX3bacvMRQZopg1VJzc0WRvNK"
     return scanpy.read(filename, backup_url=url)
+
+
+def download_prolocdata(name: str) -> AnnData:
+    from urllib.parse import urlparse
+
+    parsed_url = urlparse(name)
+    if parsed_url.scheme != "":
+        return io.read_prolocdata(parsed_url.geturl())
+    else:
+        files = list_prolocdata_files()
+        if name not in files:
+            raise ValueError(f"Invalid prolocdata file: {name}")
+        return io.read_prolocdata(files[name])
+
+
+def list_prolocdata_files():
+    # GitHub API endpoint for repository contents in the data directory
+    api_url = f"https://api.github.com/repos/lgatto/pRolocdata/contents/data"
+
+    response = requests.get(api_url)
+    response.raise_for_status()
+
+    files = {
+        os.path.splitext(item["name"])[0]: item["download_url"]
+        for item in response.json()
+        if item["type"] == "file"
+    }
+
+    return files
