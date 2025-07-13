5ded46bc57dbdd7a4be621b6cd08423f371c0aa0
Max Frank <max.frank@czbiohub.org>
2025-07-13 10:35:16 -0700

    add intersphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,6 +58,14 @@ html_theme_options = dict(
 html_logo = "_static/img/grassp_logo.png"
 issues_github_path = "czbiohub-sf/grassp"
 html_show_sphinx = False
+intersphinx_mapping = dict(
+    python=("https://docs.python.org/3", None),
+    numpy=("https://numpy.org/doc/stable", None),
+    pandas=("https://pandas.pydata.org/pandas-docs/stable", None),
+    anndata=("https://anndata.readthedocs.io/en/stable/", None),
+    scanpy=("https://scanpy.readthedocs.io/en/stable/", None),
+    scipy=("https://docs.scipy.org/doc/scipy", None),
+)
 
 intersphinx_mapping = dict(
     python=("https://docs.python.org/3", None),
