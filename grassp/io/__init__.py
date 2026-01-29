69b2bcf5773427671567f9746477ce6ef395ddb3
Max Frank <max.frank@czbiohub.org>
2026-01-29 07:20:13 -0800

    update io docstrings

--- grassp/io/__init__.py
+++ grassp/io/__init__.py
@@ -1,5 +1,4 @@
-from .read import (
-    read_alphastats,
+from .read import (  # read_alphastats,
     read_diann,
     read_fragpipe,
     read_maxquant,
--- grassp/io/read.py
+++ grassp/io/read.py
@@ -1,11 +1,6 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    import alphastats
-    from typing import Union
-
-import re
+# import re
 import urllib
 
 import anndata
@@ -13,94 +8,93 @@ import numpy as np
 import pandas as pd
 import protdata
 
+# def read_alphastats(
+#     loader: alphastats.BaseLoader,
+#     x_dtype: Union[np.dtype, type, int, float, None] = None,
+#     proteins_as_obs: bool = False,
+# ) -> anndata.AnnData:
+#     """Read proteomics data into an AnnData object.
 
-def read_alphastats(
-    loader: alphastats.BaseLoader,
-    x_dtype: Union[np.dtype, type, int, float, None] = None,
-    proteins_as_obs: bool = False,
-) -> anndata.AnnData:
-    """Read proteomics data into an AnnData object.
-
-    Parameters
-    ----------
-    loader
-        A loader object from alphastats that contains the raw proteomics data and metadata
-    x_dtype
-        Data type to use for the intensity matrix, by default None
-    proteins_as_obs
-        If True, proteins will be stored in obs (rows) rather than var (columns), by default False
+#     Parameters
+#     ----------
+#     loader
+#         A loader object from alphastats that contains the raw proteomics data and metadata
+#     x_dtype
+#         Data type to use for the intensity matrix, by default None
+#     proteins_as_obs
+#         If True, proteins will be stored in obs (rows) rather than var (columns), by default False
 
 
-    Notes
-    -----
-    The loader object must contain:
-    - rawinput: DataFrame with protein data
-    - software: Name of proteomics software used
-    - index_column: Column name containing protein identifiers
-    - intensity_column: Column name pattern for intensity values
-    - filter_columns: Columns used for filtering
-    - gene_names: Gene name mapping information
-    """
+#     Notes
+#     -----
+#     The loader object must contain:
+#     - rawinput: DataFrame with protein data
+#     - software: Name of proteomics software used
+#     - index_column: Column name containing protein identifiers
+#     - intensity_column: Column name pattern for intensity values
+#     - filter_columns: Columns used for filtering
+#     - gene_names: Gene name mapping information
+#     """
 
-    try:
-        import alphastats
-    except ImportError:
-        raise Exception(
-            "To read alphastats, please install the `alphastats` python package (pip install alphastats)."
-        )
+#     try:
+#         import alphastats
+#     except ImportError:
+#         raise Exception(
+#             "To read alphastats, please install the `alphastats` python package (pip install alphastats)."
+#         )
 
-    alphastats.DataSet._check_loader(
-        1, loader
-    )  # need to put ugly 1 because this is not specified as a staticmethod in alphapeptstats
-    rawinput = loader.rawinput
-    software = loader.software
-    index_column = loader.index_column
-    intensity_column = loader.intensity_column
-    intensity_regex = re.compile(intensity_column.replace("[sample]", ".*"))
-    filter_columns = loader.filter_columns
-    # evidence_df = loader.evidence_df
-    gene_names = loader.gene_names
+#     alphastats.DataSet._check_loader(
+#         1, loader
+#     )  # need to put ugly 1 because this is not specified as a staticmethod in alphapeptstats
+#     rawinput = loader.rawinput
+#     software = loader.software
+#     index_column = loader.index_column
+#     intensity_column = loader.intensity_column
+#     intensity_regex = re.compile(intensity_column.replace("[sample]", ".*"))
+#     filter_columns = loader.filter_columns
+#     # evidence_df = loader.evidence_df
+#     gene_names = loader.gene_names
 
-    df = rawinput.copy()
+#     df = rawinput.copy()
 
-    # get the intensity columns
-    if isinstance(intensity_column, str):
-        intensity_regex = re.compile(intensity_column.replace("[sample]", ".*"))
-        intensity_col_mask = df.columns.map(lambda x: intensity_regex.search(x) is not None)
-    else:
-        intensity_col_mask = df.columns.isin(intensity_column)
+#     # get the intensity columns
+#     if isinstance(intensity_column, str):
+#         intensity_regex = re.compile(intensity_column.replace("[sample]", ".*"))
+#         intensity_col_mask = df.columns.map(lambda x: intensity_regex.search(x) is not None)
+#     else:
+#         intensity_col_mask = df.columns.isin(intensity_column)
 
-    # Convert to anndata object
-    var = df.loc[:, ~intensity_col_mask]
-    X = df.loc[:, intensity_col_mask]
-    X = X.replace(np.nan, 0)
-    obs = pd.DataFrame(index=X.columns)
-    var.set_index(index_column, inplace=True)
-    var.index = var.index.astype(str)
-    adata = anndata.AnnData(X=X.to_numpy(dtype=x_dtype).T, var=var, obs=obs)
-    adata.obs["Intensity_col"] = adata.obs.index
-    sample_regex = re.compile(intensity_column.replace("[sample]", ""))
-    adata.obs["Sample_name"] = adata.obs.index.str.replace(sample_regex, "", regex=True)
-    adata.obs.set_index(keys="Sample_name", drop=False, inplace=True)
-    obs.index = obs.index.astype(str)
+#     # Convert to anndata object
+#     var = df.loc[:, ~intensity_col_mask]
+#     X = df.loc[:, intensity_col_mask]
+#     X = X.replace(np.nan, 0)
+#     obs = pd.DataFrame(index=X.columns)
+#     var.set_index(index_column, inplace=True)
+#     var.index = var.index.astype(str)
+#     adata = anndata.AnnData(X=X.to_numpy(dtype=x_dtype).T, var=var, obs=obs)
+#     adata.obs["Intensity_col"] = adata.obs.index
+#     sample_regex = re.compile(intensity_column.replace("[sample]", ""))
+#     adata.obs["Sample_name"] = adata.obs.index.str.replace(sample_regex, "", regex=True)
+#     adata.obs.set_index(keys="Sample_name", drop=False, inplace=True)
+#     obs.index = obs.index.astype(str)
 
-    # Proteins could either be in the rows or columns
-    if proteins_as_obs:
-        adata = adata.T
+#     # Proteins could either be in the rows or columns
+#     if proteins_as_obs:
+#         adata = adata.T
 
-    # Add properties of the experiment to uns
-    adata.uns["RawInfo"] = {
-        "software": software,
-        "filter_columns": filter_columns,
-        "gene_names": gene_names,
-    }
-    adata.uns["proteins_as_obs"] = proteins_as_obs
-    return adata
+#     # Add properties of the experiment to uns
+#     adata.uns["RawInfo"] = {
+#         "software": software,
+#         "filter_columns": filter_columns,
+#         "gene_names": gene_names,
+#     }
+#     adata.uns["proteins_as_obs"] = proteins_as_obs
+#     return adata
 
 
 def read_prolocdata(file_name: str, allow_nullable_strings: bool = False) -> anndata.AnnData:
-    """
-    Read a prolocdata file and return an AnnData object.
+    """Read a prolocdata file and return an AnnData object.
+
     Parameters
     ----------
     file_name : str
@@ -109,6 +103,7 @@ def read_prolocdata(file_name: str, allow_nullable_strings: bool = False) -> ann
         If False, convert pandas nullable StringDtype columns in obs/var to
         regular Python object-dtype strings for compatibility with older
         anndata writers (anndata<0.11). If True, keep nullable string dtype.
+
     Returns
     -------
     adata : AnnData
@@ -190,24 +185,99 @@ def read_prolocdata(file_name: str, allow_nullable_strings: bool = False) -> ann
 
 
 def read_maxquant(*args, **kwargs) -> anndata.AnnData:
-    """
-    Wrapper for protdata.read_maxquant that transposes the AnnData object.
+    """Read MaxQuant proteinGroups.txt file into an AnnData object.
+
+    This function serves as a wrapper around :func:`~protdata.io.read_maxquant`,
+    automatically transposing the data so that proteins are stored in ``.obs``
+    (rows) and samples in ``.var`` (columns), following grassp conventions.
+
+    Parameters
+    ----------
+    *args
+        Positional arguments passed to :func:`protdata.io.read_maxquant`.
+    **kwargs
+        Keyword arguments passed to :func:`protdata.io.read_maxquant`.
+
+    Returns
+    -------
+    AnnData
+        Annotated data object with proteins as observations (rows) and samples
+        as variables (columns).
+
+    See Also
+    --------
+    protdata.io.read_maxquant : Original protdata function for reading MaxQuant files.
+
+    Examples
+    --------
+    >>> import grassp as gr  # doctest: +SKIP
+    >>> adata = gr.io.read_maxquant('proteinGroups.txt')  # doctest: +SKIP
     """
     adata = protdata.io.read_maxquant(*args, **kwargs)
     return adata.T
 
 
 def read_fragpipe(*args, **kwargs) -> anndata.AnnData:
-    """
-    Wrapper for protdata.read_fragpipe that transposes the AnnData object.
+    """Read FragPipe combined_protein.tsv file into an AnnData object.
+
+    This function serves as a wrapper around :func:`~protdata.io.read_fragpipe`,
+    automatically transposing the data so that proteins are stored in ``.obs``
+    (rows) and samples in ``.var`` (columns), following grassp conventions.
+
+    Parameters
+    ----------
+    *args
+        Positional arguments passed to :func:`protdata.io.read_fragpipe`.
+    **kwargs
+        Keyword arguments passed to :func:`protdata.io.read_fragpipe`.
+
+    Returns
+    -------
+    AnnData
+        Annotated data object with proteins as observations (rows) and samples
+        as variables (columns).
+
+    See Also
+    --------
+    protdata.io.read_fragpipe : Original protdata function for reading FragPipe files.
+
+    Examples
+    --------
+    >>> import grassp as gr  # doctest: +SKIP
+    >>> adata = gr.io.read_fragpipe('combined_protein.tsv')  # doctest: +SKIP
     """
     adata = protdata.io.read_fragpipe(*args, **kwargs)
     return adata.T
 
 
 def read_diann(*args, **kwargs) -> anndata.AnnData:
-    """
-    Wrapper for protdata.read_diann that transposes the AnnData object.
+    """Read DIA-NN report.tsv file into an AnnData object.
+
+    This function serves as a wrapper around :func:`~protdata.io.read_diann`,
+    automatically transposing the data so that proteins are stored in ``.obs``
+    (rows) and samples in ``.var`` (columns), following grassp conventions.
+
+    Parameters
+    ----------
+    *args
+        Positional arguments passed to :func:`protdata.io.read_diann`.
+    **kwargs
+        Keyword arguments passed to :func:`protdata.io.read_diann`.
+
+    Returns
+    -------
+    AnnData
+        Annotated data object with proteins as observations (rows) and samples
+        as variables (columns).
+
+    See Also
+    --------
+    protdata.io.read_diann : Original protdata function for reading DIA-NN files.
+
+    Examples
+    --------
+    >>> import grassp as gr  # doctest: +SKIP
+    >>> adata = gr.io.read_diann('report.tsv')  # doctest: +SKIP
     """
     adata = protdata.io.read_diann(*args, **kwargs)
     return adata.T
